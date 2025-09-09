efbaaa12fda21da1f98b21bff8bd0963f621229a
AmeliaHeidari <ameliaheidari@gmail.com>
2025-09-08 22:19:02 -0600

    starting a1p2 and checking if number is prime or not

--- a1p2.c
+++ a1p2.c
@@ -1,3 +1,25 @@
 // Amelia Heidari - 30164762
-// Tutorial 3
+// Tutorial 11
 // Assignment 1
+
+// finds primes between lower and upper using n processes, each child gets its own range and memory block
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <math.h>
+
+#define MAX_PRIMES_PER_CHILD 1000 // this can be adjusted, just put it at 1000 for now
+
+int is_primeIint num) { // checking if number is prime or not 
+	if (num < 2) return 0;
+	for (int i = 2; i <= sqrt(num); ++i)
+		if (num % i == 0)
+			return 0;
+	return 1;
+}
+
+// sanity check, 3 arguments or it will bail out 
+
